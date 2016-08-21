fc085a01b234665e0c77db33e061805035a1f4bf
terasakisatoshi <terasakisatoshi.math@gmail.com>
2016-08-21 21:49:35 +0900

    add old new 1-2 codes

---
+++ o1-2.cs
@@ -0,0 +1,19 @@
+using System;
+
+class A 
+{
+  public string Id 
+  {
+    get
+    {
+        return "004989";
+    }
+  }
+}
+
+class Program{
+    static void Main(string[] args){
+        A a = new A();
+        Console.WriteLine(a.Id);
+    }
+}
